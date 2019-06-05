d95adf84bc291a2374bc977613fea99bbefe96cd
The Senate <35113674+Visbone@users.noreply.github.com>
2019-06-05 17:55:27 +0200

    Hello World

--- build.sbt
+++ build.sbt
@@ -1,5 +1,11 @@
+enablePlugins(ScalaJSPlugin)
+
 name := "starcrossed-valkyrie"
 
 version := "0.1"
 
 scalaVersion := "2.11.12"
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.7"
+
+libraryDependencies += "com.fbksoft" %%% "pixi-scala-js" % "1.0"
---
+++ src/main/scala/Valkyrie.scala
@@ -0,0 +1,14 @@
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+import org.scalajs.dom
+import org.scalajs.dom.html
+
+@JSExportTopLevel("Valkyrie")
+class Valkyrie {
+
+  @JSExport
+  def Client(canvas: html.Canvas):Unit = {
+    println("Hello, World")
+
+  }
+
+}
