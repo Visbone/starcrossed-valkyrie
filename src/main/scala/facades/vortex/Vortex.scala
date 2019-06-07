de90fd16e28b215f7103858659648bb56bbeb772
The Senate <35113674+Visbone@users.noreply.github.com>
2019-06-07 23:40:38 +0200

    vortex facade fixed

--- src/main/scala/facades/vortex/Vortex.scala
+++ src/main/scala/facades/vortex/Vortex.scala
@@ -1,7 +1,5 @@
 package facades.vortex
 
-import org.scalajs.dom.WebSocket
-
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSGlobal
 
@@ -13,13 +11,8 @@ object VORTEX extends js.Object {
   @js.native
   class Client extends js.Object {
     def this(url:String) = this()
-    //var ws:WebSocket = js.native
     def on(eventName:String, callback:js.Function):Int = js.native
-/*
-    def on(eventName:String, callback:(js.Object)=>Unit):Int = js.native
-    def on(eventName:String, callback:(js.Object,js.Object)=>Unit):Int = js.native
-    def on(eventName:String, callback:(js.Object,js.Object,js.Object)=>Unit):Int = js.native
-*/
+
   }
 
 }
